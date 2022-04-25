caf887f02fb44e585765f5594feb98af34931f0d
codinglk <lalit.kumar8618@gmail.com>
2022-04-25 16:06:57 +0530

    Photo App Albums Microservice

---
+++ src/main/java/com/codinglk/photoapp/api/albums/PhotoAppApiAlbumsApplication.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author codinglk
+ */
+package com.codinglk.photoapp.api.albums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAlbumsApplication.class, args);
+	}
+
+}
